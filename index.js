6b6cd30b97b9b44c6980d9a2b7bc4456e5f07e0b
Kai Lau <kailau02@gmail.com>
2021-05-14 14:49:28 -0700

    updated files

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-let inputField = document.getElementById("item-txt-field");
-let doneBtn = document.getElementById("done-btn");
-let clearBtn = document.getElementById("clear-btn");
-let previewText = document.getElementById("preview-txt");
+const inputField = document.getElementById("item-txt-field");
+const doneBtn = document.getElementById("done-btn");
+const clearBtn = document.getElementById("clear-btn");
+const previewText = document.getElementById("preview-txt");
+const dataTable = document.getElementById("data-table");
+const tableUL = document.getElementById("ul");
+
+let dataArray = [];
 
 const inputTxt = () => String(inputField.value).trim();
+
+function reloadTable(){
+    tableUL.textContent = "";
+    for(item of dataArray){
+        let newChild = document.createElement("li");
+        newChild.textContent = `${item.timeStamp} ••••••• ${item.title}`;
+        tableUL.appendChild(newChild)
+    }
+}
+
+function addItem(title){
+    if(inputTxt().length === 0){
+        alert("Text field must be filled out to search.");
+    }
+    else{
+        const date = new Date();
+        const timestamp = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+        const nObj = {
+            title: title,
+            timeStamp: timestamp
+        };
+        dataArray.push(nObj);
+        reloadTable();
+        inputField.value = "";
+    }
+}
 
 function setPreview() {
     let txtValue = inputTxt();
@@ -20,12 +50,17 @@ function setPreview() {
 inputField.addEventListener("input", setPreview);
 
 doneBtn.addEventListener("click", () => {
-    if(inputTxt().length === 0){
-        alert("Text field must be filled out to search.");
-    }
+    addItem(inputTxt());
 });
 
 clearBtn.addEventListener("click", () => {
     inputField.value = "";
     setPreview();
 })
+
+document.addEventListener("keypress", e => {
+    console.log(e.code);
+    if(e.key === "Enter"){
+        addItem(inputTxt());
+    }
+});
